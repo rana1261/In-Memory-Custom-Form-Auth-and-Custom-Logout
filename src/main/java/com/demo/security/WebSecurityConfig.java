dc187516f90a5252a2f99c92a7a756a3b84100a0
sohel Rana <ranacse2018@gmail.com>
2020-09-20 11:02:27 +0600

    basic form login and logout

---
+++ src/main/java/com/demo/security/WebSecurityConfig.java
@@ -0,0 +1,54 @@
+package com.demo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    @Override
+    protected UserDetailsService userDetailsService() {
+        UserDetails user1 = User
+                .withUsername("Sohel")
+                .password(passwordEncoder().encode("sohel123"))
+                .roles("USER")
+                .build();
+        UserDetails user2 = User
+                .withUsername("alamin")
+                .password(passwordEncoder().encode("alamin123"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests().antMatchers("/", "/home").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .usernameParameter("u").passwordParameter("p")
+                .permitAll()
+                .and()
+                .logout().permitAll();
+    }
+}
